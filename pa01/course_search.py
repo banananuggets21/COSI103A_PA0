ba50c3fe59d98b6b85eadbe4483f9b3cec622e23
James Kong <jameskong098@gmail.com>
2022-02-17 21:08:53 -0500

    updated Hiro's part

--- pa01/course_search.py
+++ pa01/course_search.py
@@ -22,6 +22,7 @@ description (filter by phrase in description)
 timeofday (filter by day and time, e.g. meets at 11 on Wed)
 code (filter by a course number/code)
 section (filter by a section number)
+time (filter by time)
 '''
 
 terms = {c['term'] for c in schedule.courses}
@@ -79,6 +80,11 @@ def topmenu():
         elif command in ['sn', 'section']:
             sectionNum = input("Enter a section number")
             schedule = schedule.sectionNum(sectionNum)
+        #7e. Create your own filter (each team member creates their own)
+        #Completed by Hiro Chen on 2/17/2022
+        elif command in ['t', 'time']:
+            time = input("enter a course time")
+            schedule = schedule.courseNum(time)
         else:
             print('command',command,'is not supported')
             continue
